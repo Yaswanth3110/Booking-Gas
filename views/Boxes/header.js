97e9c7d4881fec4f03f26b1b016dae6cc7580849
Yaswanth3110 <69066478+Yaswanth3110@users.noreply.github.com>
2021-07-25 19:35:26 +0530

    Create header.js

---
+++ views/Boxes/header.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+  </head>
+  <body>
+    <link rel="stylesheet" href="navbar.css">
+    <nav>
+      <div id="name">
+        <center>Gas Booking</center>
+      </div>
+      <div id="details">
+        
+          <div><a href="home" >Home</a></div>
+          <div><a href="orders" >My Orders</a></div>
+          <div><a href="cart" >Cart</a></div>
+          <div><a href="products" >Products</a></div>
+          <div><a href="#" >Queries</a></div>
+          <div><a href="logout" >Logout</a></div>
+        
+      </div>
+      <div id="extra" style="flex:20%;"></div>
+    </nav>
+    
+    <div class="table-responsive-sm details" >
+      <table class="table ">
+        <thead>
+          <tr>
+            <th><a href="home" >Home</a></th>
+            <th><a href="orders" >Orders</a></th>
+            <th><a href="cart" >Cart</a></th>
+            <th><a href="products" >Products</a></th>
+            <th><a href="home" >Queries</a></th>
+            <th><a href="home" >Logout</a></th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  
